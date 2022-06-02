259c20952063c6840058dac1b6916b32a659a099
FH <filip.hosek@entry-cz.com>
2022-06-02 12:00:01 +0200

    change > mainWindow

--- VectorController/MainWindow.xaml.cs
+++ VectorController/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VectorController.Processor;
+using vxlapi_NET;
 
 namespace VectorController
 {
@@ -20,9 +22,46 @@ namespace VectorController
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        CanMessageProcessor canBus = new(XLDefine.XL_HardwareType.XL_HWTYPE_VN1610, "VectorController_v1");
         public MainWindow()
         {
             InitializeComponent();
+            comboBoxOfMessageId.ItemsSource = canBus.GetListOfMsgId();
+            comboBoxOfMessageId.SelectedIndex = 0;
+        }
+
+        private void startCan_Click(object sender, RoutedEventArgs e)
+        {
+            canBus.StartReceive();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            canBus.StopReceive();
+        }
+
+        private void stopCan_Click(object sender, RoutedEventArgs e)
+        {
+            canBus.StopReceive();
+        }
+
+        private void btnGetListOfMsgId_Click(object sender, RoutedEventArgs e)
+        {
+            comboBoxOfMessageId.ItemsSource = canBus.GetListOfMsgId();
+        }
+
+        private void btnSetMsgIdFilter_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(comboBoxOfMessageId.SelectedItem.ToString()))
+            {
+                canBus.SetMessageIdFilter(comboBoxOfMessageId.SelectedItem.ToString());
+            }
+        }
+
+        internal void ShowCurrentMessage(string text)
+        {
+            textBoxOutputForMessage.Text = text;
         }
     }
 }
