0c7cdc766e34988842dcfe21d704d466d0912368
FH <filip.hosek@entry-cz.com>
2022-08-16 10:21:06 +0200

    Tx test - OK

--- VectorController/Processor/CanFdBus.cs
+++ VectorController/Processor/CanFdBus.cs
@@ -71,14 +71,14 @@ namespace VectorController.Processor
             GetXlDriverConfiguration();
 
 
-            RunRxThread();
+            //RunRxThread();
 
 
-            //for (int i = 0; i < 20; i++)
-            //{
-            //    CanFdTransmit();
+            for (int i = 0; i < 20; i++)
+            {
+                CanFdTransmit();
 
-            //}
+            }
         }
 
 
