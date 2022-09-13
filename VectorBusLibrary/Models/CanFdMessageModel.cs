0bd5c5d5c965b4722013eb9a260ad42086e95bf4
FH <filip.hosek@entry-cz.com>
2022-09-13 12:26:28 +0200

    imporve dataTypes canBus model

--- VectorBusLibrary/Models/CanFdMessageModel.cs
+++ VectorBusLibrary/Models/CanFdMessageModel.cs
@@ -1,6 +1,23 @@
-﻿namespace VectorBusLibrary.Models
+﻿using System;
+using vxlapi_NET;
+
+namespace VectorBusLibrary.Models
 {
-    internal class CanFdMessageModel
+    public class CanFdMessageModelRx
     {
+        public uint canId { get; set; }  //CAN ID.
+        public uint msgFlags { get; set; }
+        public uint crc { get; set; }  //Crc of the CAN message.
+        public ulong reserved1 { get; set; }
+        public ushort totalBitCnt { get; set; }  //Number of received bits including stuff bit.
+        public XLDefine.XL_CANFD_DLC dlc { get; set; }  //4-bit data length code.
+        public byte reserved { get; set; }  //Internal use.
+        public byte[] data { get; set; }  //Data that was received.
+
+    }
+
+    public class CanFdMessageModelTx 
+    {
+
     }
 }
