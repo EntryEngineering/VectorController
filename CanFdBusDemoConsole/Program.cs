166d1a8dd19ef7f0bbd742368f0d90022a9baa0a
FH <filip.hosek@entry-cz.com>
2022-09-13 07:23:20 +0200

    add can and canfd demo console project

---
+++ CanFdBusDemoConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CanFdBusDemoConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
