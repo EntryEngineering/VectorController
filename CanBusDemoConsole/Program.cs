d16260cb1c0aaa6d077b2e0527a4ef2036ef471d
FH <filip.hosek@entry-cz.com>
2022-09-13 07:52:09 +0200

    can and canFd console app - RX ok

--- CanBusDemoConsole/Program.cs
+++ CanBusDemoConsole/Program.cs
@@ -8,7 +8,7 @@ namespace CanBusDemoConsole
     {
         static void Main(string[] args)
         {
-            CanBus canBus = new(new XLDriver(), XLDefine.XL_HardwareType.XL_HWTYPE_VN1610,"CanBusTestAppConsole");
+            CanBus canBus = new(new XLDriver(), XLDefine.XL_HardwareType.XL_HWTYPE_VN1610, "Can Bus-Test");
             canBus.TestCanBus();
         }
     }
--- CanFdBusDemoConsole/Program.cs
+++ CanFdBusDemoConsole/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace CanFdBusDemoConsole
+﻿using System;
+using VectorBusLibrary.Processors;
+using vxlapi_NET;
+
+namespace CanFdBusDemoConsole
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CanFdBus canFdBus = new CanFdBus(new XLDriver(), XLDefine.XL_HardwareType.XL_HWTYPE_VN1610, "CanFD Bus-Test");
+            canFdBus.TestCanFDBus();
         }
     }
 }
