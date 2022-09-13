96f43c5223351576a602b8a9237eeb885c4b9e42
FH <filip.hosek@entry-cz.com>
2022-09-13 08:40:03 +0200

    can bus demo console - test rx - ok

--- CanBusDemoConsole/Program.cs
+++ CanBusDemoConsole/Program.cs
@@ -1,15 +1,43 @@
-﻿using System;
-using VectorBusLibrary.Processors;
+﻿using VectorBusLibrary.Processors;
 using vxlapi_NET;
 
 namespace CanBusDemoConsole
 {
     internal class Program
     {
+        private static CanBus canBus;
+
         static void Main(string[] args)
         {
-            CanBus canBus = new(new XLDriver(), XLDefine.XL_HardwareType.XL_HWTYPE_VN1610, "Can Bus-Test");
-            canBus.TestCanBus();
+            canBus = new(new XLDriver(), XLDefine.XL_HardwareType.XL_HWTYPE_VN1610, "Can Bus-Test");
+            InitCanControloler();
+            canBus.RunRxThread();
+            Console.WriteLine("RX msg:");
+            while (true)
+            {
+                Console.WriteLine(canBus.OutMsg);
+            }
+        }
+
+
+
+        internal static void InitCanControloler()
+        {
+            Console.WriteLine("****************************");
+            Console.WriteLine("CanBus - Vector");
+            Console.WriteLine("****************************");
+
+            Console.WriteLine("vxlapi_NET        : " + typeof(XLDriver).Assembly.GetName().Version);
+            canBus.OpenDriver();
+            canBus.GetDriverConfig();
+            canBus.GetAppConfigAndSetAppConfig();
+            canBus.RequestTheUserToAssignChannels();
+            CommonVector.GetAccesMask();
+            Console.WriteLine(CommonVector.PrintAccessMask());
+            canBus.OpenPort();
+            canBus.ActivateChannel();
+            canBus.SetNotificationCanBus();
+            canBus.ResetClock();
         }
     }
 }
