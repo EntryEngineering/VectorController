eb8b79e0fbbae4c98860e86140f95a527cad220f
Filip Hošek <filip.hosek@entry-cz.com>
2022-09-15 13:36:37 +0200

    change crc8_c2_lookUpTable1dArray and test GetValueFromTable

--- TestCrc/Program.cs
+++ TestCrc/Program.cs
@@ -5,7 +5,22 @@
         static void Main(string[] args)
         {
             VectorBusLibrary.Processors.CrcProcessor crc = new();
-            Console.WriteLine(crc.GetCrc("0x1AFFD4FF16FFC1"));
+            //Console.WriteLine(crc.GetCrc("0x1AFFD4FF16FFC1"));
+
+            while (true)
+            {
+                int indexFromConsole = Int32.Parse(Console.ReadLine());
+                if (indexFromConsole <= 255)
+                {
+                    Console.WriteLine($"Result is {string.Format("0x{0:x}", crc.GetValueFromTable(indexFromConsole))}");
+
+                }
+                else
+                {
+                    Console.WriteLine("Out of range");
+                }
+            }
+
         }
     }
 }
