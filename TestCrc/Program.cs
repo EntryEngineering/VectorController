08b0dd25bdef5162d5438022c13bacdad605056b
FH <filip.hosek@entry-cz.com>
2022-09-19 07:19:42 +0200

    add console test crc

--- TestCrc/Program.cs
+++ TestCrc/Program.cs
@@ -9,14 +9,35 @@
             crc = new();
             //Console.WriteLine(crc.GetCrc("0x1AFFD4FF16FFC1"));
 
-            Console.WriteLine($"{crc.GetCrc("1AFFD4FF16FFC1")}");
+            TestCrc();
             Console.ReadLine();
         }
 
-        private static void TestTable() 
+
+        private static int TestCrc()
         {
             while (true)
             {
+                Console.Write($"Enter message: ");
+                string message = Console.ReadLine();
+                if (message.Length == 14)
+                {
+                    Console.WriteLine($"CRC is: {crc.GetCrc(message)}");
+
+                }
+                else
+                {
+                    Console.WriteLine($"Out of range");
+                }
+
+            }
+        }
+
+        private static void TestTable()
+        {
+            while (true)
+            {
+
                 int indexFromConsole = Int32.Parse(Console.ReadLine());
                 if (indexFromConsole <= 255)
                 {
