c617a2ac786b0a7541fbaa9c99e4ff5534900675
FH <filip.hosek@entry-cz.com>
2022-09-26 08:49:43 +0200

    add helper class

--- CanBusDemoWpf/MainWindow.xaml.cs
+++ CanBusDemoWpf/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using VectorBusLibrary.Processors;
+using vxlapi_NET;
 
 namespace CanBusDemoWpf
 {
@@ -20,9 +12,47 @@ namespace CanBusDemoWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static CanBus? canBus = null;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnIntitDriver_Click(object sender, RoutedEventArgs e)
+        {
+            string appName = txtBoxAppName.Text;
+            if (string.IsNullOrEmpty(appName))
+            {
+                MessageBox.Show("Please enter app name");
+            }
+            else
+            {
+                canBus = new(new XLDriver(), XLDefine.XL_HardwareType.XL_HWTYPE_VN1610, appName);
+                Helelper.WriteLogToTextBox("Driver init - Ok", txtBoxLogApp);
+
+            }
+        }
+
+        private void btnOpenDriver_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnOpenPort_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    public static class Helelper
+    {
+        public static void WriteLogToTextBox(string text, TextBox uiElement)
+        {
+            string outString = $"{DateTime.Now:G}>>{text}{Environment.NewLine}";
+
+            Trace.WriteLine(outString);
+            uiElement.AppendText(outString);
         }
     }
 }
