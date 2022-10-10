bee2cd00a9bf5b20030df70dd8efae9c41d09561
FH <filip.hosek@entry-cz.com>
2022-10-10 10:30:52 +0200

    cleanCode

--- VectorRestApi/Controllers/VectorBusController.cs
+++ VectorRestApi/Controllers/VectorBusController.cs
@@ -4,8 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace VectorRestApi.Controllers
 {
 
