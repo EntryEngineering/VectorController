072ca12df26cb38f8e0438a4be066c499fb318ce
FH <filip.hosek@entry-cz.com>
2022-10-17 09:25:26 +0200

    add new modes

---
+++ VectorRestApi/Model/MessageModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace VectorRestApi.Model
+{
+    public class MessageModel
+    {
+        public uint MessageId { get; set; }
+
+        public ushort DLC { get; set; }
+
+        public List<SignalModel> Signals { get; set; }
+    }
+}
---
+++ VectorRestApi/Model/SignalModel.cs
@@ -0,0 +1,10 @@
+﻿namespace VectorRestApi.Model
+{
+    public class SignalModel
+    {
+        public string SignalName { get; set; }
+        public ushort StartBit { get; set; }
+        public ushort Lengh { get; set; }
+        public string binaryData { get; set; }
+    }
+}
