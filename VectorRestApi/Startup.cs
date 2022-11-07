20a96e60ea5d36e239893b3545c35afb760fe697
Filip Hošek <filip.hosek@entry-cz.com>
2022-11-07 07:22:29 +0100

    up ws

--- VectorRestApi/Startup.cs
+++ VectorRestApi/Startup.cs
@@ -9,6 +9,9 @@ using System;
 using System.Net.WebSockets;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Net;
+using System.Text;
+using System.Diagnostics;
 
 namespace VectorRestApi
 {
@@ -46,29 +49,52 @@ namespace VectorRestApi
 
             app.UseRouting();
 
-            var webSocketsOptions = new WebSocketOptions { KeepAliveInterval = TimeSpan.FromSeconds(120) };
 
-            app.UseWebSockets(webSocketsOptions);
 
-            app.Use(async (context, next) =>
-            {
-                if (context.Request.Path == "/testWs")
+            var wsOptions = new WebSocketOptions { KeepAliveInterval = TimeSpan.FromSeconds(120) };
+            app.UseWebSockets(wsOptions);
+            app.Use(async (context, next) => {
+                if (context.Request.Path == "/send")
                 {
                     if (context.WebSockets.IsWebSocketRequest)
                     {
-                        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
-                        await MyWebSocketHandler(context, webSocket);
+                        using (WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync())
+                        {
+                            await Send(context, webSocket);
+                        }
                     }
                     else
                     {
-                        context.Response.StatusCode = 400;
+                        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
                     }
                 }
-                else
-                {
-                    await next();
-                }
             });
+
+
+
+            //var webSocketsOptions = new WebSocketOptions { KeepAliveInterval = TimeSpan.FromSeconds(120) };
+
+            //app.UseWebSockets(webSocketsOptions);
+
+            //app.Use(async (context, next) =>
+            //{
+            //    if (context.Request.Path == "/testWs")
+            //    {
+            //        if (context.WebSockets.IsWebSocketRequest)
+            //        {
+            //            WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+            //            await MyWebSocketHandler(context, webSocket);
+            //        }
+            //        else
+            //        {
+            //            context.Response.StatusCode = 400;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        await next();
+            //    }
+            //});
 
             app.UseAuthorization();
 
@@ -78,17 +104,36 @@ namespace VectorRestApi
             });
         }
 
-        private async Task MyWebSocketHandler(HttpContext context, WebSocket webSocket)
+
+        private async Task Send(HttpContext context, WebSocket webSocket)
         {
             var buffer = new byte[1024 * 4];
-            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-            while (!result.CloseStatus.HasValue)
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), System.Threading.CancellationToken.None);
+            if (result != null)
             {
-                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
-
-                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                while (!result.CloseStatus.HasValue)
+                {
+                    string msg = Encoding.UTF8.GetString(new ArraySegment<byte>(buffer, 0, result.Count));
+                    Trace.WriteLine($"client says: {msg}");
+                    await webSocket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes($"Server says {DateTime.UtcNow:f}")), result.MessageType, result.EndOfMessage, System.Threading.CancellationToken.None);
+                    result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), System.Threading.CancellationToken.None);
+                    Trace.WriteLine(result);
+                }
             }
-            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, System.Threading.CancellationToken.None);
         }
+
+        //private async Task MyWebSocketHandler(HttpContext context, WebSocket webSocket)
+        //{
+        //    var buffer = new byte[1024 * 4];
+        //    WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        //    while (!result.CloseStatus.HasValue)
+        //    {
+        //        await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+        //        result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        //    }
+        //    await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        //}
     }
 }
